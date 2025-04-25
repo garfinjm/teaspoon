3d4d7145b09f1ca845b9dd5f32bef2a2cb3b8a5f
Jake Garfin <garfinjm@gmail.com>
2025-04-25 09:47:26 -0500

    Add ability to ignore files based on prefix, added multi-threading to tablespoon.py

--- tablespoon.py
+++ tablespoon.py
@@ -1,96 +1,143 @@
 #!/usr/bin/env python3
 
+import argparse
+import concurrent.futures
 import os
 import sys
-import argparse
 from teaspoon import subsample_reads  # Import the subsample_reads function
 
-def downsample_paired_reads(desired_coverage, name_type, input_dir, output_dir):
-  """
-  Downsamples paired-end Illumina reads in a input_dir using teaspoon.py.
 
-  Args:
-    desired_coverage (int): The desired coverage for downsampling.
-    name_type (str): Downsampled file name format choose: prepend, insert, extend
-    input_dir (str): Path to the directory containing the paired-end reads.
-    output_dir (str): Path to the directory where downsampled reads will be saved.
-  """
-  if not os.path.isdir(input_dir):
-    print(f"Error: {input_dir} is not a valid directory.")
-    sys.exit(1)
-  if not os.path.isdir(output_dir):
-    os.makedirs(output_dir)
-  # Check name_type is one of: prepend, insert, extend
-  if name_type not in ["prepend", "insert", "extend"]:
-    print("Error: name_type must be one of: prepend, insert, extend")
-    sys.exit(1)
-  if not isinstance(desired_coverage, int) or desired_coverage <= 0:
-    print("Error: Desired coverage must be a positive integer.")
-    sys.exit(1)
+def downsample_paired_reads(
+    desired_coverage, name_type, input_dir, exclude, output_dir, threads
+):
+    """
+    Downsamples paired-end Illumina reads in a input_dir using teaspoon.py.
 
-  # Find pairs of reads in input input_dir that match illumina MiSeq/NextSeq naming schemes and NCBI fasterq-dump outputs
-  read_pairs = []
-  for filename in os.listdir(input_dir):
-    if filename.endswith(".fastq") or filename.endswith(".fastq.gz"):
-      # Check if the file is paired-end
-      if "_1." in filename or "_R1_" in filename:
-        read1_path = os.path.join(input_dir, filename)
-        read2_path = read1_path.replace("_1.", "_2.").replace("_R1_", "_R2_")
-        if os.path.isfile(read2_path):
-          read_pairs.append((read1_path, read2_path))
+    Args:
+      desired_coverage (int): The desired coverage for downsampling.
+      name_type (str): Downsampled file name format choose: prepend, insert, extend
+      input_dir (str): Path to the directory containing the paired-end reads.
+      exclude (str): Exclude fastq files that start with this string.
+      output_dir (str): Path to the directory where downsampled reads will be saved.
+      threads (int): Number of threads to use for downsampling.
+    """
+    if not os.path.isdir(input_dir):
+        print(f"Error: {input_dir} is not a valid directory.")
+        sys.exit(1)
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    # Check name_type is one of: prepend, insert, extend
+    if name_type not in ["prepend", "insert", "extend"]:
+        print("Error: name_type must be one of: prepend, insert, extend")
+        sys.exit(1)
+    if not isinstance(desired_coverage, int) or desired_coverage <= 0:
+        print("Error: Desired coverage must be a positive integer.")
+        sys.exit(1)
+    if not isinstance(threads, int) or threads <= 0:
+        print("Error: Number of threads must be a positive integer.")
+        sys.exit(1)
 
-  if not read_pairs:
-    print("No paired-end reads found in the input directory.")
-    sys.exit(1)
+    # Find pairs of reads in input input_dir that match illumina MiSeq/NextSeq naming schemes and NCBI fasterq-dump outputs
+    read_pairs = []
+    for filename in os.listdir(input_dir):
+        if (filename.endswith(".fastq") or filename.endswith(".fastq.gz")) and (
+            exclude is None or not filename.startswith(exclude)
+        ):
+            # Check if the file is paired-end
+            if "_1." in filename or "_R1_" in filename:
+                read1_path = os.path.join(input_dir, filename)
+                read2_path = read1_path.replace("_1.", "_2.").replace("_R1_", "_R2_")
+                if os.path.isfile(read2_path):
+                    read_pairs.append((read1_path, read2_path))
 
-  # Process each pair of reads
-  for read1_path, read2_path in read_pairs:
-    try:
-      # Call the teaspoon.py subsample_reads function
-      subsample_reads(
-        desired_coverage, name_type, read1_path, read2_path, output_dir
-      )
-    except Exception as e:
-      print(f"Error processing {read1_path} and {read2_path}: {e}")
-      sys.exit(1)
-  print(
-      f"Successfully downsampled reads to {desired_coverage}x coverage and saved to {output_dir}"
-  )
+    if not read_pairs:
+        print("No paired-end reads found in the input directory.")
+        sys.exit(1)
+
+    def process_read_pair(read1_path, read2_path):
+        """
+        Helper function to process a single read pair.
+        """
+        try:
+            subsample_reads(
+                desired_coverage, name_type, read1_path, read2_path, output_dir
+            )
+
+        except Exception as e:
+            print(f"Error processing {read1_path} and {read2_path}: {e}")
+            # sys.exit(1) #Do not exit, report error and continue
+            raise e  # Re-raise exception to be handled by executor
+
+    # Parallel processing of read pairs
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        futures = [executor.submit(process_read_pair, r1, r2) for r1, r2 in read_pairs]
+
+        # Handle exceptions from threads
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()  # If exception occurred, it will be raised here
+            except Exception as e:
+                print(f"A thread raised an exception: {e}")
+
+    print(
+        f"Successfully downsampled reads to {desired_coverage}x coverage and saved to {output_dir}"
+    )
 
 
 if __name__ == "__main__":
-  
-  parser = argparse.ArgumentParser(
-    description="Subsample reads in a directory to a specified (rough) coverage using teaspoon."
-  )
-  parser.add_argument(
-    "-n",
-    "--name_type",
-    required=False,
-    default="prepend",
-    type=str,
-    help="Downsampled file name format (Default: prepend). Choose: prepend, insert, extend",
-  )
-  parser.add_argument(
-    "-i",
-    "--input_dir",
-    required=True,
-    help="Path to the directory containing the input fastq files."
-  )
-  parser.add_argument(
-    "-o",
-    "--output_dir",
-    required=False,
-    default=".",
-    help="Output path for downsampled fastq files."
-  )
-  parser.add_argument(
-    "-c",
-    "--coverage",
-    required=True,
-    type=int,
-    help="Desired approximate coverage for subsampling."
-  )
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Subsample reads in a directory to a specified (rough) coverage using teaspoon."
+    )
+    parser.add_argument(
+        "-n",
+        "--name_type",
+        required=False,
+        default="prepend",
+        type=str,
+        help="Downsampled file name format (Default: prepend). Choose: prepend, insert, extend",
+    )
+    parser.add_argument(
+        "-i",
+        "--input_dir",
+        required=True,
+        help="Path to the directory containing the input fastq files.",
+    )
+    parser.add_argument(
+        "-e",
+        "--exclude",
+        required=False,
+        help="Exclude fastq files that start with this string.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        required=False,
+        default=".",
+        help="Output path for downsampled fastq files.",
+    )
+    parser.add_argument(
+        "-c",
+        "--coverage",
+        required=True,
+        type=int,
+        default=100,
+        help="Desired approximate coverage for subsampling (default: 100).",
+    )
+    parser.add_argument(
+        "-t",
+        "--threads",
+        required=False,
+        type=int,
+        default=8,
+        help="Number of threads to use for downsampling (default: 8).",
+    )
+    args = parser.parse_args()
 
-  downsample_paired_reads(args.coverage, args.name_type, args.input_dir, args.output_dir)
+    downsample_paired_reads(
+        args.coverage,
+        args.name_type,
+        args.input_dir,
+        args.exclude,
+        args.output_dir,
+        args.threads,
+    )
--- teaspoon.py
+++ teaspoon.py
@@ -3,150 +3,162 @@
 import argparse
 import os
 import re
+import shutil
 import subprocess
 
 
 def subsample_reads(desired_coverage, name_type, r1, r2, output_dir):
-  """
-  Subsamples paired-end reads using mash and rasusa.
+    """
+    Subsamples paired-end reads using mash and rasusa.
 
-  Args:
-    desired_coverage (int): Desired coverage for subsampling.
-    name_type (str): Downsampled file name format choose: prepend, insert, extend
-    r1 (str): Path to the first read file.
-    r2 (str): Path to the second read file.
-    output_dir (str): Directory to save the downsampled files.
-  """
-  if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-  if not os.path.isfile(r1) or not os.path.isfile(r2):
-    raise FileNotFoundError("One or both read files do not exist.")
-  if not isinstance(desired_coverage, int) or desired_coverage <= 0:
-    raise ValueError("Desired coverage must be a positive integer.")
-  if name_type not in ["prepend", "insert", "extend"]:
-    raise ValueError("name_type must be one of: prepend, insert, extend")
+    Args:
+      desired_coverage (int): Desired coverage for subsampling.
+      name_type (str): Downsampled file name format choose: prepend, insert, extend
+      r1 (str): Path to the first read file.
+      r2 (str): Path to the second read file.
+      output_dir (str): Directory to save the downsampled files.
+    """
 
-  # Create the output directory if it doesn't exist
-  if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
+    if not os.path.isfile(r1) or not os.path.isfile(r2):
+        raise FileNotFoundError("One or both read files do not exist.")
+    if not isinstance(desired_coverage, int) or desired_coverage <= 0:
+        raise ValueError("Desired coverage must be a positive integer.")
+    if name_type not in ["prepend", "insert", "extend"]:
+        raise ValueError("name_type must be one of: prepend, insert, extend")
 
-  r1_filename = os.path.basename(r1)
-  r2_filename = os.path.basename(r2)
-  padded_coverage = str(desired_coverage).zfill(3)
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-  if name_type == "prepend":
-    print("Using prepend naming scheme")
-    r1_out = os.path.join(output_dir, f"{padded_coverage}xds-{r1_filename}")
-    r2_out = os.path.join(output_dir, f"{padded_coverage}xds-{r2_filename}")
-  elif name_type == "insert":
-    print("Using insert naming scheme")
-    r1_out = os.path.join(
-      output_dir,
-      f"{r1_filename.split('_', 1)[0]}-{padded_coverage}xds_{r1_filename.split('_', 1)[1]}",
-      )
-    r2_out = os.path.join(
-      output_dir,
-      f"{r2_filename.split('_', 1)[0]}-{padded_coverage}xds_{r2_filename.split('_', 1)[1]}",
-    )
-  elif name_type == "extend":
-    print("Using extend naming scheme")
-    r1_out = os.path.join(
-      output_dir,
-      re.sub(r"(.fastq.gz|.fastq)", f".{padded_coverage}xds\\1", r1_filename),
-    )
-    r2_out = os.path.join(
-      output_dir,
-      re.sub(r"(.fastq.gz|.fastq)", f".{padded_coverage}xds\\1", r2_filename),
-    )
-  else:
-    print("Unrecognized naming scheme, defaulting to prepend")
-    r1_out = os.path.join(output_dir, f"{padded_coverage}xds-{r1_filename}")
-    r2_out = os.path.join(output_dir, f"{padded_coverage}xds-{r2_filename}")
+    r1_filename = os.path.basename(r1)
+    r2_filename = os.path.basename(r2)
+    padded_coverage = str(desired_coverage).zfill(3)
 
-  # Run mash, capturing stderr
-  mash_command = [
-    "mash",
-    "sketch",
-    "-o",
-    "/dev/null",
-    "-k",
-    "21",
-    "-m",
-    "10",
-    "-r",
-    "-",
-  ]
-  with open(r1, "rb") as r1_file, open(r2, "rb") as r2_file:
-    mash_process = subprocess.run(
-      mash_command, input=r1_file.read() + r2_file.read(), capture_output=True
-    )
+    if name_type == "prepend":
+        print("Using prepend naming scheme")
+        r1_out = os.path.join(output_dir, f"{padded_coverage}xds-{r1_filename}")
+        r2_out = os.path.join(output_dir, f"{padded_coverage}xds-{r2_filename}")
+    elif name_type == "insert":
+        print("Using insert naming scheme")
+        r1_out = os.path.join(
+            output_dir,
+            f"{r1_filename.split('_', 1)[0]}-{padded_coverage}xds_{r1_filename.split('_', 1)[1]}",
+        )
+        r2_out = os.path.join(
+            output_dir,
+            f"{r2_filename.split('_', 1)[0]}-{padded_coverage}xds_{r2_filename.split('_', 1)[1]}",
+        )
+    elif name_type == "extend":
+        print("Using extend naming scheme")
+        r1_out = os.path.join(
+            output_dir,
+            re.sub(r"(.fastq.gz|.fastq)", f".{padded_coverage}xds\\1", r1_filename),
+        )
+        r2_out = os.path.join(
+            output_dir,
+            re.sub(r"(.fastq.gz|.fastq)", f".{padded_coverage}xds\\1", r2_filename),
+        )
+    else:
+        print("Unrecognized naming scheme, defaulting to prepend")
+        r1_out = os.path.join(output_dir, f"{padded_coverage}xds-{r1_filename}")
+        r2_out = os.path.join(output_dir, f"{padded_coverage}xds-{r2_filename}")
 
-  # Capture estimated genome size and coverage from mash stderr
-  genome_size = None
-  coverage = None
-  for line in mash_process.stderr.decode().splitlines():
-    if line.startswith("Estimated genome size:"):
-      genome_size = float(line.split(":")[1].strip())
-    elif line.startswith("Estimated coverage:"):
-      coverage = float(line.split(":")[1].strip())
-      coverage = round(coverage)
+    print(f"Processing {r1_filename} and {r2_filename}")
 
-  if genome_size is None or coverage is None:
-    raise ValueError("Could not parse genome size and coverage from mash output.")
+    # Copy the fastq files directly if either in the pair are empty(ish)
+    if os.stat(r1).st_size <= 100 or os.stat(r2).st_size <= 100:
+        print(
+            f"Warning: {r1} or {r2} is effectively empty. Copying the files without downsampling."
+        )
+        shutil.copy(r1, r1_out)
+        shutil.copy(r2, r2_out)
+        return
 
-  print(f"Processing {r1_filename} and {r2_filename}")
+    # Run mash, capturing stderr
+    mash_command = [
+        "mash",
+        "sketch",
+        "-o",
+        "/dev/null",
+        "-k",
+        "21",
+        "-m",
+        "10",
+        "-r",
+        "-",
+    ]
+    with open(r1, "rb") as r1_file, open(r2, "rb") as r2_file:
+        mash_process = subprocess.run(
+            mash_command, input=r1_file.read() + r2_file.read(), capture_output=True
+        )
 
-  # Run rasusa
-  rasusa_command = [
-    "rasusa",
-    "reads",
-    "-s",
-    "11327544032246541232",
-    "-g",
-    str(genome_size),
-    "-c",
-    str(desired_coverage),  # Use desired_coverage here
-    "-o",
-    r1_out,
-    "-o",
-    r2_out,
-    r1,
-    r2,
-  ]
-  subprocess.run(rasusa_command, check=True)
+    # Capture estimated genome size and coverage from mash stderr
+    genome_size = None
+    coverage = None
+    for line in mash_process.stderr.decode().splitlines():
+        if line.startswith("Estimated genome size:"):
+            genome_size = float(line.split(":")[1].strip())
+        elif line.startswith("Estimated coverage:"):
+            coverage = float(line.split(":")[1].strip())
+            coverage = round(coverage)
+
+    if genome_size is None or coverage is None:
+        raise ValueError("Could not parse genome size and coverage from mash output.")
+
+    # Run rasusa
+    rasusa_command = [
+        "rasusa",
+        "reads",
+        "-s",
+        "11327544032246541232",
+        "-g",
+        str(genome_size),
+        "-c",
+        str(desired_coverage),
+        "-o",
+        r1_out,
+        "-o",
+        r2_out,
+        r1,
+        r2,
+    ]
+    subprocess.run(rasusa_command, check=True)
+
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(
-    description="Subsample reads to an approximate coverage using mash and rasusa."
-  )
-  parser.add_argument(
-    "-c",
-    "--coverage",
-    required=True,
-    type=int,
-    help="Desired coverage for subsampling.",
-  )
-  parser.add_argument(
-    "-n",
-    "--name_type",
-    required=False,
-    default="prepend",
-    type=str,
-    help="Downsampled file name format (Default: prepend). Choose: prepend, insert, extend",
-  )
-  parser.add_argument(
-    "-r1", "--read1", required=True, help="Path to the first read file."
-  )
-  parser.add_argument(
-    "-r2", "--read2", required=True, help="Path to the second read file."
-  )
-  parser.add_argument(
-    "-o",
-    "--output_dir",
-    required=False,
-    default=".",
-    help="Directory to save the downsampled files.",
-  )
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Subsample reads to an approximate coverage using mash and rasusa."
+    )
+    parser.add_argument(
+        "-c",
+        "--coverage",
+        required=True,
+        type=int,
+        help="Desired coverage for subsampling.",
+    )
+    parser.add_argument(
+        "-n",
+        "--name_type",
+        required=False,
+        default="prepend",
+        type=str,
+        help="Downsampled file name format (Default: prepend). Choose: prepend, insert, extend",
+    )
+    parser.add_argument(
+        "-r1", "--read1", required=True, help="Path to the first read file."
+    )
+    parser.add_argument(
+        "-r2", "--read2", required=True, help="Path to the second read file."
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        required=False,
+        default=".",
+        help="Directory to save the downsampled files.",
+    )
+    args = parser.parse_args()
 
-  subsample_reads(args.coverage, args.name_type, args.read1, args.read2, args.output_dir)
+    subsample_reads(
+        args.coverage, args.name_type, args.read1, args.read2, args.output_dir
+    )
